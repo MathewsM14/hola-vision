7cbfd7d814cd9c2e89575558389d50ccef0a9b77
MathewsM14 <matmarquez@unal.edu.co>
2025-04-08 08:21:15 -0500

    matplot1

--- import matplotlib.py
+++ import matplotlib.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-I = np.array([[0, 255, 0],[0, 255, 0],[0, 255, 0]])
+I = np.array([[255, 255, 255],[0, 255, 0],[0, 255, 0]])
 plt.imshow(I, vmin=0, vmax=255)
 plt.show()
