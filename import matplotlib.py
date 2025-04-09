3b4b2f544930ef15d69454f4df4c03b638600273
MathewsM14 <matmarquez@unal.edu.co>
2025-04-09 17:17:45 -0500

    up

--- import matplotlib.py
+++ import matplotlib.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-I = np.array([[255, 255, 255],[0, 255, 0],[0, 255, 0]])
+#I = np.array([[255, 255, 255],[0, 255, 0],[0, 255, 0]])
+#plt.imshow(I, vmin=0, vmax=255)
+#plt.show()
+
+I = np.array([[[0, 255, 0], [0, 128, 128]], 
+                [[128, 0, 0], [128, 0, 128] ]], dtype=np.uint8)
+
 plt.imshow(I, vmin=0, vmax=255)
 plt.show()
